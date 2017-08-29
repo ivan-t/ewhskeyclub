14cda9f5d609878c782bee1cd173774e000de663
Ivan Tan <ivanxtan@gmail.com>
2017-08-28 20:22:30 -0700

    Made certain fields in login and register forms to be case insensitive

--- account/forms.py
+++ account/forms.py
@@ -9,8 +9,8 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
 
     def clean(self):
-        username = self.cleaned_data.get("username")
-        password = self.cleaned_data.get("password")
+        username = self.cleaned_data.get("username").lower()
+        password = self.cleaned_data.get("password").lower()
 
         if username and password:
             user = authenticate(username=username, password=password)
@@ -23,7 +23,7 @@ class LoginForm(forms.Form):
         return super(LoginForm, self).clean()
 
 class RegisterForm(forms.ModelForm):
-    letters_only = RegexValidator(r'^[a-zA-Z]+$', 'This field must contain only letters.')
+    letters_only = RegexValidator(r'^[a-zA-Z\-]+$', 'This field cannot have spaces and must contain only letters or hypens.')
     alphanumeric = RegexValidator(r'^[a-zA-Z0-9]+$', 'This field must contain only letters or numbers.')
 
     username = forms.Field(label='Username', validators=[alphanumeric], widget=forms.TextInput(attrs={'placeholder':'Username'}))
@@ -46,9 +46,25 @@ class RegisterForm(forms.ModelForm):
             'confirm_password',
         ]
 
+    def clean_username(self):
+        username = self.cleaned_data.get('username').lower()
+        return username
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name').lower()
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name').lower()
+        return last_name
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email').lower()
+        return email
+
     def clean_confirm_email(self):
-        email = self.cleaned_data.get('email')
-        confirm_email = self.cleaned_data.get('confirm_email')
+        email = self.cleaned_data.get('email').lower()
+        confirm_email = self.cleaned_data.get('confirm_email').lower()
         if email != confirm_email:
             raise forms.ValidationError("Emails must match.")
         email_qs = User.objects.filter(email=email)
