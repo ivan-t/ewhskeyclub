2e46bc76577d907fe0767f438e178e070f3323ed
Ivan Tan <ivanxtan@gmail.com>
2017-09-04 21:54:24 -0700

    Added redirects for favicon files

--- ewhskc/urls.py
+++ ewhskc/urls.py
@@ -16,10 +16,21 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 import events.views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^android-chrome-192x192.png', RedirectView.as_view(url='/static/favicon/android-chrome-192x192.png', permanent=True)),
+    url(r'^android-chrome-384x384.png', RedirectView.as_view(url='/static/favicon/android-chrome-384x384.png', permanent=True)),
+    url(r'^apple-touch-icon.png', RedirectView.as_view(url='/static/favicon/apple-touch-icon.png', permanent=True)),
+    url(r'^browserconfig.xml', RedirectView.as_view(url='/static/favicon/browserconfig.xml', permanent=True)),
+    url(r'^favicon.ico', RedirectView.as_view(url='static/favicon/favicon.ico', permanent=True)),
+    url(r'^favicon-16x16.png', RedirectView.as_view(url='/static/favicon/favicon-16x16.png', permanent=True)),
+    url(r'^favicon-32x32.png', RedirectView.as_view(url='/static/favicon/favicon-32x32.png', permanent=True)),
+    url(r'^manifest.json', RedirectView.as_view(url='/static/favicon/manifest.json', permanent=True)),
+    url(r'^mstile-150x150.png', RedirectView.as_view(url='/static/favicon/mstile-150x150.png', permanent=True)),
+    url(r'^safari-pinned-tab.svg', RedirectView.as_view(url='/static/favicon/safari-pinned-tab.svg', permanent=True)),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('account.urls')),
     url(r'^profile/', events.views.profile, name="profile"),
